e5ce39a374d1ce25b51928ab3c5bb839e37fe5a6
hg <devnull@localhost>
2014-03-24 08:31:02 +0100

    Use new element_properties utility functions.

--- src/pyhmsa/spec/condition/elementalid.py
+++ src/pyhmsa/spec/condition/elementalid.py
@@ -19,26 +19,9 @@
 from pyhmsa.spec.condition.condition import _Condition
 from pyhmsa.util.parameter import \
     NumericalAttribute, TextAttribute, AtomicNumberAttribute
+from pyhmsa.util.element_properties import get_symbol, get_atomic_number
 
 # Globals and constants variables.
-
-_SYMBOLS = [
-        "H"  , "He" , "Li" , "Be" , "B"  , "C"  , "N"  , "O",
-        "F"  , "Ne" , "Na" , "Mg" , "Al" , "Si" , "P"  , "S",
-        "Cl" , "Ar" , "K"  , "Ca" , "Sc" , "Ti" , "V"  , "Cr",
-        "Mn" , "Fe" , "Co" , "Ni" , "Cu" , "Zn" , "Ga" , "Ge",
-        "As" , "Se" , "Br" , "Kr" , "Rb" , "Sr" , "Y"  , "Zr",
-        "Nb" , "Mo" , "Tc" , "Ru" , "Rh" , "Pd" , "Ag" , "Cd",
-        "In" , "Sn" , "Sb" , "Te" , "I"  , "Xe" , "Cs" , "Ba",
-        "La" , "Ce" , "Pr" , "Nd" , "Pm" , "Sm" , "Eu" , "Gd",
-        "Tb" , "Dy" , "Ho" , "Er" , "Tm" , "Yb" , "Lu" , "Hf",
-        "Ta" , "W"  , "Re" , "Os" , "Ir" , "Pt" , "Au" , "Hg",
-        "Tl" , "Pb" , "Bi" , "Po" , "At" , "Rn" , "Fr" , "Ra",
-        "Ac" , "Th" , "Pa" , "U"  , "Np" , "Pu" , "Am" , "Cm",
-        "Bk" , "Cf" , "Es" , "Fm" , "Md" , "No" , "Lr" , "Rf",
-        "Db" , "Sg" , "Bh" , "Hs" , "Mt" , "Ds" , "Rg" , "Cn",
-        "Uut", "Fl" , "Uup", "Lv" , "Uus", "Uuo"
-    ]
 
 class ElementalID(_Condition):
 
@@ -61,10 +44,10 @@ class ElementalID(_Condition):
         """
         Returns the symbol.
         """
-        return _SYMBOLS[self.atomic_number - 1]
+        return get_symbol(self.atomic_number)
 
     def set_symbol(self, symbol):
-        self.atomic_number = _SYMBOLS.index(symbol) + 1
+        self.atomic_number = get_atomic_number(symbol)
 
     symbol = property(get_symbol, set_symbol, doc='Symbol')
 
