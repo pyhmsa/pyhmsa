210c0a671f4f13a9891b5eb855cd4130f3acbe45
hg <devnull@localhost>
2014-04-04 15:15:08 +0200

    Implement __call__ for calibration to retrieve the value of the calibration at a certain index.

--- src/pyhmsa/spec/condition/calibration.py
+++ src/pyhmsa/spec/condition/calibration.py
@@ -14,6 +14,7 @@
 # Standard library modules.
 
 # Third party modules.
+import numpy as np
 
 # Local modules.
 from pyhmsa.util.parameter import \
@@ -22,23 +23,26 @@ from pyhmsa.util.parameter import \
 # Globals and constants variables.
 
 class _Calibration(Parameter):
-    
+
     quantity = TextAttribute(True, 'Quantity', 'physical quantity')
     unit = UnitAttribute(None, True, 'Unit', 'unit')
 
     def __init__(self, quantity, unit):
         """
-        Describes the calibration of a set of measurement ordinals with respect 
-        to a physical quantity, such as converting channels in an EELS spectrum 
-        to energy, or steps in a WDS peak scan to position, angle, wavelength 
+        Describes the calibration of a set of measurement ordinals with respect
+        to a physical quantity, such as converting channels in an EELS spectrum
+        to energy, or steps in a WDS peak scan to position, angle, wavelength
         or energy.
-        
-        :arg quantity: physical quantity such as "Energy", "Wavelength", 
+
+        :arg quantity: physical quantity such as "Energy", "Wavelength",
             "Position", etc. (required)
         :arg unit: unit (required)
         """
         self.quantity = quantity
         self.unit = unit
+
+    def __call__(self, index):
+        raise NotImplementedError
 
 class CalibrationConstant(_Calibration):
 
@@ -46,17 +50,20 @@ class CalibrationConstant(_Calibration):
 
     def __init__(self, quantity, unit, value):
         """
-        Defines the energy/wavelength/etc calibration of a spectrometer or other 
-        measurement device operating at a fixed position, such as a CL 
+        Defines the energy/wavelength/etc calibration of a spectrometer or other
+        measurement device operating at a fixed position, such as a CL
         monochromator.
-        
+
         :arg quantity: physical quantity (required)
         :arg unit: unit (required)
         :arg value: value (required)
         """
         _Calibration.__init__(self, quantity, unit)
-        
+
         self.value = value
+
+    def __call__(self, index):
+        return self.value
 
 class CalibrationLinear(_Calibration):
 
@@ -65,15 +72,15 @@ class CalibrationLinear(_Calibration):
 
     def __init__(self, quantity, unit, gain, offset):
         """
-        Defines the energy/wavelength/etc calibration of a spectrometer or 
-        other measurement device, for which the measurement ordinals (e.g. 
-        channel numbers) have a linear relationship to the physical quantity 
+        Defines the energy/wavelength/etc calibration of a spectrometer or
+        other measurement device, for which the measurement ordinals (e.g.
+        channel numbers) have a linear relationship to the physical quantity
         (e.g. nm), with a constant offset and gain.
-        
+
         :arg quantity: physical quantity (required)
         :arg unit: unit (required)
         :arg gain: gain (required)
-        :arg offset: offset, the calibration value (energy, wavelength, 
+        :arg offset: offset, the calibration value (energy, wavelength,
             position, etc.) corresponding to the first measurement ordinal
             (required)
         """
@@ -82,17 +89,20 @@ class CalibrationLinear(_Calibration):
         self.gain = gain
         self.offset = offset
 
+    def __call__(self, index):
+        return np.poly1d([self.gain, self.offset])(index)
+
 class CalibrationPolynomial(_Calibration):
 
     coefficients = NumericalAttribute(None, True, 'Coefficients', 'polynomial coefficients')
 
     def __init__(self, quantity, unit, coefficients):
         """
-        Defines the energy/wavelength/etc calibration of a spectrometer or 
-        other measurement device, for which the measurement ordinals (e.g. 
-        channel numbers) have a relationship to the physical quantity (e.g. nm) 
+        Defines the energy/wavelength/etc calibration of a spectrometer or
+        other measurement device, for which the measurement ordinals (e.g.
+        channel numbers) have a relationship to the physical quantity (e.g. nm)
         that may be modelled by an nth order polynomial.
-        
+
         :arg quantity: physical quantity (required)
         :arg unit: unit (required)
         :arg coefficients: iterable of coefficients (required)
@@ -100,20 +110,23 @@ class CalibrationPolynomial(_Calibration):
         _Calibration.__init__(self, quantity, unit)
 
         self.coefficients = coefficients
-    
+
+    def __call__(self, index):
+        return np.poly1d(self.coefficients)(index)
+
 class CalibrationExplicit(_Calibration):
 
     values = NumericalAttribute(None, True, "Values", "explicit values")
 
     def __init__(self, quantity, unit, values):
         """
-        Defines the energy/wavelength/etc calibration of a spectrometer or 
-        other measurement device, for which relationship between the measurement 
-        ordinals (e.g. channel numbers) and physical quantity (e.g. nm) cannot 
-        be adequately modelled by linear or polynomial functions, and therefore 
-        must be declared explicitly for each ordinal as an array of floating 
+        Defines the energy/wavelength/etc calibration of a spectrometer or
+        other measurement device, for which relationship between the measurement
+        ordinals (e.g. channel numbers) and physical quantity (e.g. nm) cannot
+        be adequately modelled by linear or polynomial functions, and therefore
+        must be declared explicitly for each ordinal as an array of floating
         point values.
-        
+
         :arg quantity: physical quantity (required)
         :arg unit: unit (required)
         :arg values: explicit values (required)
@@ -121,3 +134,6 @@ class CalibrationExplicit(_Calibration):
         _Calibration.__init__(self, quantity, unit)
 
         self.values = values
+
+    def __call__(self, index):
+        return self.values[index]
--- src/pyhmsa/spec/condition/test_calibration.py
+++ src/pyhmsa/spec/condition/test_calibration.py
@@ -4,6 +4,7 @@
 # Standard library modules.
 import unittest
 import logging
+import pickle
 
 # Third party modules.
 
@@ -31,6 +32,13 @@ class Test_Calibration(unittest.TestCase):
         self.assertRaises(ValueError, _Calibration, None, 'eV')
         self.assertRaises(ValueError, _Calibration, 'Energy', None)
 
+    def testpickle(self):
+        s = pickle.dumps(self.cal)
+        cal = pickle.loads(s)
+
+        self.assertEqual('Energy', cal.quantity)
+        self.assertEqual('eV', cal.unit)
+
 class TestCalibrationConstant(unittest.TestCase):
 
     def setUp(self):
@@ -43,8 +51,20 @@ class TestCalibrationConstant(unittest.TestCase):
 
     def testskeleton(self):
         self.assertAlmostEqual(-237.098251, self.cal.value, 4)
+        self.assertAlmostEqual(-237.098251, self.cal(0), 4)
+        self.assertAlmostEqual(-237.098251, self.cal(1), 4)
 
         self.assertRaises(ValueError, CalibrationConstant, 'Energy', 'eV', None)
+
+    def testpickle(self):
+        s = pickle.dumps(self.cal)
+        cal = pickle.loads(s)
+
+        self.assertEqual('Energy', cal.quantity)
+        self.assertEqual('eV', cal.unit)
+        self.assertAlmostEqual(-237.098251, cal.value, 4)
+        self.assertAlmostEqual(-237.098251, cal(0), 4)
+        self.assertAlmostEqual(-237.098251, cal(1), 4)
 
 class TestCalibrationLinear(unittest.TestCase):
 
@@ -59,9 +79,22 @@ class TestCalibrationLinear(unittest.TestCase):
     def testskeleton(self):
         self.assertAlmostEqual(2.49985, self.cal.gain, 4)
         self.assertAlmostEqual(-237.098251, self.cal.offset, 4)
+        self.assertAlmostEqual(-237.098251, self.cal(0), 4)
+        self.assertAlmostEqual(-237.098251 + 2.49985, self.cal(1), 4)
 
         self.assertRaises(ValueError, CalibrationLinear, 'Energy', 'eV', None, -237.098251)
         self.assertRaises(ValueError, CalibrationLinear, 'Energy', 'eV', 2.49985, None)
+
+    def testpickle(self):
+        s = pickle.dumps(self.cal)
+        cal = pickle.loads(s)
+
+        self.assertEqual('Energy', cal.quantity)
+        self.assertEqual('eV', cal.unit)
+        self.assertAlmostEqual(2.49985, self.cal.gain, 4)
+        self.assertAlmostEqual(-237.098251, self.cal.offset, 4)
+        self.assertAlmostEqual(-237.098251, cal(0), 4)
+        self.assertAlmostEqual(-237.098251 + 2.49985, cal(1), 4)
 
 class TestCalibrationPolynomial(unittest.TestCase):
 
@@ -79,8 +112,23 @@ class TestCalibrationPolynomial(unittest.TestCase):
         self.assertAlmostEqual(0.677, self.cal.coefficients[1], 4)
         self.assertAlmostEqual(0.134, self.cal.coefficients[2], 4)
         self.assertAlmostEqual(-0.018, self.cal.coefficients[3], 4)
+        self.assertAlmostEqual(-0.018, self.cal(0), 4)
+        self.assertAlmostEqual(-1.462, self.cal(1), 4)
 
         self.assertRaises(ValueError, CalibrationPolynomial, 'Energy', 'eV', None)
+
+    def testpickle(self):
+        s = pickle.dumps(self.cal)
+        cal = pickle.loads(s)
+
+        self.assertEqual('Energy', cal.quantity)
+        self.assertEqual('eV', cal.unit)
+        self.assertAlmostEqual(-2.255, cal.coefficients[0], 4)
+        self.assertAlmostEqual(0.677, cal.coefficients[1], 4)
+        self.assertAlmostEqual(0.134, cal.coefficients[2], 4)
+        self.assertAlmostEqual(-0.018, cal.coefficients[3], 4)
+        self.assertAlmostEqual(-0.018, cal(0), 4)
+        self.assertAlmostEqual(-1.462, cal(1), 4)
 
 class TestCalibrationExplicit(unittest.TestCase):
 
@@ -98,8 +146,27 @@ class TestCalibrationExplicit(unittest.TestCase):
         self.assertAlmostEqual(0.677, self.cal.values[1], 4)
         self.assertAlmostEqual(0.134, self.cal.values[2], 4)
         self.assertAlmostEqual(-0.018, self.cal.values[3], 4)
+        self.assertAlmostEqual(-2.255, self.cal(0), 4)
+        self.assertAlmostEqual(0.677, self.cal(1), 4)
+        self.assertAlmostEqual(0.134, self.cal(2), 4)
+        self.assertAlmostEqual(-0.018, self.cal(3), 4)
 
         self.assertRaises(ValueError, CalibrationExplicit, 'Energy', 'eV', None)
+
+    def testpickle(self):
+        s = pickle.dumps(self.cal)
+        cal = pickle.loads(s)
+
+        self.assertEqual('Energy', cal.quantity)
+        self.assertEqual('eV', cal.unit)
+        self.assertAlmostEqual(-2.255, cal.values[0], 4)
+        self.assertAlmostEqual(0.677, cal.values[1], 4)
+        self.assertAlmostEqual(0.134, cal.values[2], 4)
+        self.assertAlmostEqual(-0.018, cal.values[3], 4)
+        self.assertAlmostEqual(-2.255, cal(0), 4)
+        self.assertAlmostEqual(0.677, cal(1), 4)
+        self.assertAlmostEqual(0.134, cal(2), 4)
+        self.assertAlmostEqual(-0.018, cal(3), 4)
 
 if __name__ == '__main__': # pragma: no cover
     logging.getLogger().setLevel(logging.DEBUG)
