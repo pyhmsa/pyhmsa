5313bcc39dd87c0106111dac9b755198c2868795
hg <devnull@localhost>
2014-09-16 10:10:14 +0200

    Update to version 0.1.1.

--- pyhmsa/__init__.py
+++ pyhmsa/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Philippe T. Pinard"
 __email__ = "philippe.pinard@gmail.com"
-__version__ = "0.1"
+__version__ = "0.1.1"
 __copyright__ = "Copyright (c) 2013-2014 Philippe T. Pinard"
 __license__ = "MIT"
 
