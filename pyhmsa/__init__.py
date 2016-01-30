ccae75f9c7cceaeda0cff86f176d1291af084773
Philippe Pinard <philippe.pinard@gmail.com>
2016-01-30 14:25:47 +0100

    Use versioneer.

--- pyhmsa/__init__.py
+++ pyhmsa/__init__.py
@@ -5,9 +5,12 @@ Data File specification
 
 __author__ = "Philippe T. Pinard"
 __email__ = "philippe.pinard@gmail.com"
-__version__ = "0.1.9"
 __copyright__ = "Copyright (c) 2013-2015 Philippe T. Pinard"
 __license__ = "MIT"
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 # This is required to create a namespace package.
 # A namespace package allows programs to be located in different directories or
--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 # Standard library modules.
 import os
-import re
 import sys
 import glob
 import codecs
@@ -13,31 +12,10 @@ from subprocess import check_call
 from setuptools import setup, find_packages
 
 # Local modules.
+import versioneer
 
 # Globals and constants variables.
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
-
-def find_version(*file_paths):
-    """
-    Read the version number from a source file.
-
-    .. note::
-
-       Why read it, and not import?
-       see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
-    """
-    # Open in Latin-1 so that we avoid encoding errors.
-    # Use codecs.open for Python 2 compatibility
-    with codecs.open(os.path.join(BASEDIR, *file_paths), 'r', 'latin1') as f:
-        version_file = f.read()
-
-    # The version line must have the form
-    # __version__ = 'ver'
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
 
 class _bdist_fpm(Command):
 
@@ -93,8 +71,11 @@ with codecs.open('README.rst', encoding='utf-8') as f:
 
 packages = find_packages()
 
+cmdclass = versioneer.get_cmdclass()
+cmdclass['bdist_deb'] = bdist_deb
+
 setup(name='pyHMSA',
-      version=find_version('pyhmsa', '__init__.py'),
+      version=versioneer.get_version(),
       description='Python implementation of the MSA / MAS / AMAS Hyper-Dimensional Data File specification',
       long_description=long_description,
 
@@ -192,5 +173,5 @@ setup(name='pyHMSA',
              'RAW = pyhmsa.fileformat.exporter.raw:ExporterRAW'],
         },
 
-      cmdclass={'bdist_deb': bdist_deb},
+      cmdclass=cmdclass,
      )
